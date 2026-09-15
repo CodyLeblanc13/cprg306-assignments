24376264e0742430310e818f8e8bff7d6a94f712
Alyssa DaCosta <alyssalrd@gmail.com>
2026-09-14 18:12:21 -0600

    changes for assignment 2

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import StudentInfo from "./student-info";
+
+export default function Page() {
+  return (
+    <main>
+        <h1>Shopping List</h1>
+        <StudentInfo />
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+// student-info.js
+export default function StudentInfo() {
+  return (
+    <div className="student-info">
+      <h2>Alyssa DaCosta</h2>
+      <a className="text-pink-600 underline"
+        href="https://github.com/grimoiresandgithb" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        View my GitHub Repository
+      </a>
+    </div>
+  );
+}
