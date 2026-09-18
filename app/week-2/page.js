21e75974916c27dc10f45f55fe60ddd7654d2804
Yassine <hammoudamessaoudi123@gmail.com>
2026-09-18 16:32:21 -0700

    Ahmed Yassine Messaoudi's work

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,11 +1,10 @@
-import Home from "../home";
 import StudentInfo from "./student-info";
+
 export default function Page() {
-  return (
-    <main>
-      <h1>Shopping List</h1>
-      <StudentInfo/>
-      <Home/>
-    </main>
-  );
+    return(
+        <main>
+            <h1>Shopping List</h1>
+            <StudentInfo></StudentInfo>
+        </main>
+    );
 }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 export default function StudentInfo() {
-  return (
-    <main>
-      <h1>Cody Leblanc</h1>
-      <Link href="https://github.com/CodyLeblanc13/cprg306-assignments" className="text-blue-500 text-underline">GitHub repo link</Link>
-    </main>
-  );
+    return(
+        <div>
+            <h1>Ahmed Yassine Messaoudi</h1>
+            <Link className = "text-underline text-blue-700" href="https://github.com/CodyLeblanc13/cprg306-assignments">GitHub Repository</Link>
+        </div>
+    );
 }
