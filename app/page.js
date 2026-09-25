3703ece3c7aa199547ea2c528c061c9c237d8c40
CodyLeblanc13 <cody.leblanc@edu.sait.ca>
2026-09-25 15:09:18 -0600

    Changed homepage styling

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 export default function Page() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold text-purple-600 text-center p-4">cprg306-assignments</h1>
-      <p className="text-center pb-4">Click one of the following links to view the weekly assignment</p>
-      <section className="text-center">
+    <main className="p-6">
+      <h1 className="text-3xl font-bold text-purple-600">CPRG306 Assignments</h1>
+      <p className="pb-4">Click one of the following links to view the weekly assignment</p>
+      <section>
       <Link href="/week-2" className="text-underline text-blue-500">
         Go to Week 2
         <p className="text-white ">My name and github link</p>
